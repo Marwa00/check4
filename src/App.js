7157a2511d0ed36251f84b01bd3a84986e17a15c
unknown <Marwa00>
2019-12-21 12:28:43 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -2,23 +2,30 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import photo from './photo';
+
 function App() {
   return (
+
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Wrapper>
+
+<Photo src=”C:\Users\marwa\Desktop/ABC.png” />
+
+<Title style={{
+
+  color:’red’
+
+}}>My Name here</Title>
+
+<Title small>My job here</Title>
+
+
+</Wrapper>
+
+
+
     </div>
   );
 }
---
+++ src/photo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+function Photo(props) {
+    
+    
+    return(
+    <div>
+     <img src={props.img} />
+
+
+    </div>)
+
+
+}
+
+export default Photo ; 
